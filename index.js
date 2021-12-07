7a1124a71fea39cc9c9050e42a8129179f2624c5
Eaindra Wun Pyae <38712207+eaindra-wp@users.noreply.github.com>
2021-12-07 09:07:35 +0100

    replaced the cropped image to be dragged/dropped with transparent text

--- index.js
+++ index.js
@@ -5,37 +5,117 @@ var org_image_url = "./1.jpg";
 var canvas = (this.__canvas = new fabric.Canvas("canvas", {
   hoverCursor: "pointer",
   selection: false,
-  targetFindTolerance: 2
+  targetFindTolerance: 2,
+  backgroundColor: "green"
 }));
+
+// removing borders to Fabric items
 
 // adding background image (i.e. image with text boundings) to the canvas
 fabric.Image.fromURL(org_image_url, function (img) {
   img.set({
     top: 0,
     left: 0,
-    scaleX: canvas.getWidth() / img.width, //new update
+    scaleX: canvas.getWidth() / img.width / 2, //new update
     scaleY: canvas.getHeight() / img.height //new update
   });
   canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
     // Optionally add an opacity lvl to the image
-    backgroundImageOpacity: 0.5
+    backgroundImageOpacity: 0.3
   });
 
   // leaving blank after the text has been dragged to the category div
-  var rect = new fabric.Rect({
-    left: (244 * canvas.getWidth()) / img.width,
+  /* var rect = new fabric.Rect({
+    left: (244 * canvas.getWidth()) / 2 / img.width,
     top: (103 * canvas.getHeight()) / img.height,
     width: 369,
     height: 52,
-    scaleX: canvas.getWidth() / img.width, //new update
+    scaleX: canvas.getWidth() / 2 / img.width, //new update
     scaleY: canvas.getHeight() / img.height, //new update
     fill: "#fff",
     globalCompositeOperation: "destination-out",
     lockMovementX: true,
-    lockMovementY: true
+    lockMovementY: true,
+    selectable: false
   });
-  canvas.add(rect);
+  canvas.add(rect);  */
 });
+
+function getImageDimensions(img_url) {
+  var img = new Image();
+  img.src = img_url;
+  var img_dims = {
+    img_width: img.width,
+    img_height: img.height
+  };
+  return img_dims;
+}
+
+const img_dims = getImageDimensions(org_image_url);
+
+// calculating the text's position on the image, and stack on the canvas background.
+// fabric.Image.fromURL(org_image_url, function (myImg) {
+//   var img1 = myImg.set({
+//     left: (244 * canvas.getWidth()) / 2 / myImg.width,
+//     top: (103 * canvas.getHeight()) / myImg.height,
+//     width: 369,
+//     height: 52,
+//     scaleX: canvas.getWidth() / myImg.width / 2, //new update
+//     scaleY: canvas.getHeight() / myImg.height, //new update
+//     borderColor: "green",
+//     lockMovementX: true,
+//     lockMovementY: true,
+//     selectable: false,
+//     customLayer: 0
+//   });
+
+//   img1.cropX = 244; // cropping image in x-axis
+//   img1.cropY = 103; // cropping image in y-axis
+//   canvas.add(img1);
+// });
+
+var text = new fabric.Textbox("MORENO", {
+  left: (244 * canvas.getWidth()) / 2 / img_dims.img_width,
+  top: (103 * canvas.getHeight()) / img_dims.img_height,
+  width: 369,
+  height: 52,
+  scaleX: canvas.getWidth() / img_dims.img_width / 2, //new update
+  scaleY: canvas.getHeight() / img_dims.img_height, //new update
+  borderColor: "green",
+  fontSize: 25,
+  fill: "transparent",
+  textAlign: "center",
+  customLayer: 1,
+  scalable: false
+});
+canvas.add(text);
+
+canvas.on("mouse:over", function (e) {
+  e.target.set({
+    backgroundColor: "blue",
+    fill: "white"
+  });
+});
+
+canvas.on("mouse:out", function (e) {
+  e.target.set({
+    backgroundColor: "transparent"
+  });
+});
+
+/* var brand_rect = new fabric.Rect({
+  left: 520,
+  top: 0,
+  width: 480,
+  height: 200,
+  fill: "#9042f5",
+  globalCompositeOperation: "destination-out",
+  lockMovementX: true,
+  lockMovementY: true,
+  selectable: false,
+  borderColor: "blue"
+});
+canvas.add(brand_rect); */
 
 // this is for the 'discard' button on each draggable image.
 /* var deleteIcon =
@@ -59,35 +139,18 @@ fabric.Object.prototype.controls.deleteControl = new fabric.Control({
   cornerSize: 24
 }) */
 
-// calculating the text's position on the image, and stack on the canvas background.
-fabric.Image.fromURL(org_image_url, function (myImg) {
-  var img1 = myImg.set({
-    left: (244 * canvas.getWidth()) / myImg.width,
-    top: (103 * canvas.getHeight()) / myImg.height,
-    width: 369,
-    height: 52,
-    scaleX: canvas.getWidth() / myImg.width, //new update
-    scaleY: canvas.getHeight() / myImg.height, //new update
-    borderColor: "green"
-  });
+// function deleteObject(eventData, transform) {
+//   var target = transform.target;
+//   var canvas = target.canvas;
+//   canvas.remove(target);
+//   canvas.requestRenderAll();
+// }
 
-  img1.cropX = 244; // cropping image in x-axis
-  img1.cropY = 103; // cropping image in y-axis
-  canvas.add(img1);
-});
-
-function deleteObject(eventData, transform) {
-  var target = transform.target;
-  var canvas = target.canvas;
-  canvas.remove(target);
-  canvas.requestRenderAll();
-}
-
-function renderIcon(ctx, left, top, styleOverride, fabricObject) {
-  var size = this.cornerSize;
-  ctx.save();
-  ctx.translate(left, top);
-  ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
-  ctx.drawImage(img, -size / 2, -size / 2, size, size);
-  ctx.restore();
-}
+// function renderIcon(ctx, left, top, styleOverride, fabricObject) {
+//   var size = this.cornerSize;
+//   ctx.save();
+//   ctx.translate(left, top);
+//   ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
+//   ctx.drawImage(img, -size / 2, -size / 2, size, size);
+//   ctx.restore();
+// }
